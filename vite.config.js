2ac46f4e45b4115c427df4c1a212f89105dfbbdd
BarbaraBravoRedondo <barbarabravoredondo@gmail.com>
2023-11-20 13:55:38 +0100

    adding

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/promo-U-module-3-pair-2-sprint-2-hangman-game/",
+  base: "/Hangman/",
 })
